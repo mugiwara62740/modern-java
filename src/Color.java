7422b5715b6e580de50ed3c96e354b1b219d8557
EL OUAHMANI <younes.el-ouahmani@capgemini.com>
2019-03-22 14:24:09 +0100

    Test code auchan

--- src/Color.java
+++ src/Color.java
@@ -1,4 +1,4 @@
 
 public enum Color {
-	RED, GREEN
+	RED, GREEN, BLUE
 }
--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class Test {
 
@@ -15,45 +13,11 @@ public class Test {
 
 		List<Apple> inventory = new ArrayList<Apple>(
 				Arrays.asList(a, b, c, d, e));
-		List<Integer> numbers = new ArrayList<Integer>(
-				Arrays.asList(1, 12, 13, 1234, 122));
-		System.out.println("Avant filtrage : " + inventory.toString());
-		List<Apple> redApples = filter(inventory,
-				(Apple apple) -> Color.RED.equals(apple.getColor()));
-		List<Integer> evenNumbers = filter(numbers, (Integer i) -> i % 2 == 0);
-		inventory.sort(new Comparator<Apple>() {
-			public int compare(Apple a1, Apple a2) {
-				return a1.getWeight().compareTo(a2.getWeight());
-			}
 
-		});
-
-		numbers.sort((Integer a1, Integer a2) -> a1.compareTo(a2));
-		System.out.println("Après filtrage Rouge : " + redApples.toString());
-		System.out.println(
-				"Après filtrage Nombre Pair: " + evenNumbers.toString());
-		System.out.println("Après comparaison poids: " + inventory.toString());
-
-		System.out.println("Après comparaison Numbers: " + numbers.toString());
-
-		Thread t1 = new Thread(new Runnable() {
-			public void run() {
-				System.out.println("Hello world 1");
-			}
-		});
-		t1.run();
-
-		Thread t2 = new Thread(() -> System.out.println("Hello world 2"));
-		t2.run();
-	}
-
-	public static <T> List<T> filter(List<T> inventory, Predicate<T> predicat) {
-		List<T> result = new ArrayList<>();
-		for (T item : inventory) {
-			if (predicat.test(item)) {
-				result.add(item);
-			}
-		}
-		return result;
+		inventory.stream().filter(apple -> apple.getColor().equals(Color.BLUE))
+				.findFirst().map(appleBlue -> {
+					System.out.println(appleBlue.getColor().toString());
+					return appleBlue.getColor();
+				});
 	}
 }
